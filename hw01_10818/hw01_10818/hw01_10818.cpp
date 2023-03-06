7ce27d3c4e9de6eff3d2219e75d48a72daeef580
Kim hajeong <happykhj67@gmail.com>
2023-03-07 02:45:30 +0900

    hw01 10818

---
+++ hw01_10818/hw01_10818/hw01_10818.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+	ios_base::sync_with_stdio(0);
+
+	int N;
+	cin >> N;
+
+	int array[100001];
+
+	for (int i = 0; i < N; i++) {
+		cin >> array[i];
+	}
+
+	sort(array, array + N);
+
+	cout << array[0] << " " << array[N - 1];
+
+	return 0;
+
+}
